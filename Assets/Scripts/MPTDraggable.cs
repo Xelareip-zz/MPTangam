fa5f443c1c6bddce7a5e70481871b3a26604cd1b
Xelareip <piergrimault@gmail.com>
2016-07-16 15:07:59 +0200

    Revert "Adds drag delay + can touch everywhere"
    This reverts commit 973bb1f012148a002b58baa0e8185e607561fba4.

--- Assets/Scripts/MPTDraggable.cs
+++ Assets/Scripts/MPTDraggable.cs
@@ -7,9 +7,6 @@ public class MPTDraggable : MonoBehaviour
     public bool currentlyDragged;
     private Vector3 initialPos;
     private Collider2D coll;
-    public float dragDelay = 0.5f;
-    public float dragStartTime;
-
 
     public event Action beenDragged;
     public event Action beenDropped;
@@ -25,9 +22,11 @@ public class MPTDraggable : MonoBehaviour
     {
         Vector2 initPosition = new Vector2(float.MinValue, float.MinValue);
         Vector2 position = initPosition;
+        bool justClicked = false;
 #if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
         {
+            justClicked = true;
             position = Input.mousePosition;
         }
         else if (Input.GetMouseButton(0))
@@ -37,12 +36,15 @@ public class MPTDraggable : MonoBehaviour
 #endif
         if (Input.touchCount == 1)
         {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                justClicked = true;
+            }
             position = Input.touches[0].position;
         }
 
         if (position == initPosition)
         {
-            dragStartTime = Time.time + dragDelay;
             if (currentlyDragged)
             {
                 if (beenDragged != null)
@@ -56,20 +58,22 @@ public class MPTDraggable : MonoBehaviour
 
         position = MPHUtils.ScreenToWorld(position);
         
-        
-        if (dragStartTime <= Time.time)
+        if (coll.OverlapPoint(position))
         {
-            currentlyDragged = true;
+            if (justClicked)
+            {
+                currentlyDragged = true;
+            }
         }
 
         if (currentlyDragged)
         {
             transform.position = new Vector3(position.x, position.y, 0.0f);
-            if (beenDragged != null)
-            {
-                beenDragged();
-            }
         }
 
+        if (beenDragged != null)
+        {
+            beenDragged();
+        }
     }
 }
