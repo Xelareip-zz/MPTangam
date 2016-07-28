8b6f16153f07525543dc936563c96bf36a30cf6b
Xelareip <piergrimault@gmail.com>
2016-07-28 21:39:42 +0200

    Adds intro scene

---
+++ Assets/Scripts/MPTSplashScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MPTSplashScene : MonoBehaviour
+{
+    public float delay;
+    public string nextScene;
+    
+    void Start ()
+    {
+        StartCoroutine(LoadNextInDelay());
+	}
+	
+	IEnumerator LoadNextInDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadSceneAsync(nextScene);
+	}
+}
