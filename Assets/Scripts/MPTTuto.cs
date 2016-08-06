c0cb31e4d5891873674a3045dc5a24c309a3c910
Xelareip <piergrimault@gmail.com>
2016-08-06 18:45:22 +0200

    Missing files from last commit

---
+++ Assets/Scripts/MPTTuto.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MPTTuto : MonoBehaviour
+{
+    public List<GameObject> listOfPages;
+
+    public int currentPage;
+
+    public void OnEnabled()
+    {
+        currentPage = 0;
+        for (int pageId = 0; pageId < listOfPages.Count; ++pageId)
+        {
+            listOfPages[pageId].SetActive(false);
+        }
+    }
+
+    public void PageClicked()
+    {
+        ++currentPage;
+        for (int pageId = 0; pageId < listOfPages.Count && pageId < currentPage; ++pageId)
+        {
+            listOfPages[pageId].SetActive(false);
+        }
+
+        if (currentPage >= listOfPages.Count)
+        {
+            currentPage = 0;
+            gameObject.SetActive(false);
+            MPTGameManager.Instance.Resume();
+        }
+    }
+}
