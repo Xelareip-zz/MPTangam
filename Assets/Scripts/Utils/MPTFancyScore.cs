f0c227cac1ef0a3590b3fbd0157eb03511de6631
Xelareip <piergrimault@gmail.com>
2016-08-06 17:57:35 +0200

    Fancy score fades out

--- Assets/Scripts/Utils/MPTFancyScore.cs
+++ Assets/Scripts/Utils/MPTFancyScore.cs
@@ -7,18 +7,29 @@ public class MPTFancyScore : MonoBehaviour
     public float speed;
     public Vector3 direction;
     public float lifeTime;
+    public float fadeOutTime;
 
     private float startTime;
+    private TextMesh textMesh;
 
     void Start()
     {
         direction = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.forward) * Vector3.up;
         startTime = Time.time;
+        textMesh = GetComponent<TextMesh>();
     }
 
     void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
+
+        if (startTime + lifeTime - fadeOutTime < Time.time)
+        {
+            if (textMesh)
+            {
+                textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, (startTime + lifeTime - Time.time) / fadeOutTime);
+            }
+        }
         if (startTime + lifeTime < Time.time)
         {
             Destroy(gameObject);
